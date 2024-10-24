273dd68ba0739b43446be7d731d16a85799a8a43
Turtle0815 <39406946+Turtle0815@users.noreply.github.com>
2024-10-24 10:30:27 +0200

    Update Boersenpreis.py

--- logic/Boersenpreis.py
+++ logic/Boersenpreis.py
@@ -4,4 +4,11 @@ class Boersenpreis():
     
     def Funktion_Preis():
         Preis_aktuell = 10
+        Datum_aktuell = 2024-10-24
+        Uhrzeit_aktuell = 15-00-00
         return Preis_aktuell
+        return Datum_aktuell
+        return Uhrzeit_aktuell
+
+# Die entsprechenden Daten müssen zum entsprechenden Zeitpunkt aus der API 
+# gezogen werden. Sie werden dann von Preisgenerator.py weiterverarbeitet.
